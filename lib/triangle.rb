5e62d6336e9e54aa1eb93f2b8a2b358484f3d583
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-27 01:16:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,29 @@
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
+  def initialize(s1,s2,s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+  
+  def kind()
+    if (@s1 <= 0) || (@s2 <= 0) || (@s3 <= 0)
+      raise TriangleError
+      elsif (@s1+@s2 <= @s3) || (@s1+@s3 <= @s2) || (@s2+@s3 <= @s1)
+      raise TriangleError
+    else
+      if (@s1 == @s2) &&(@s2 == @s3)
+        :equilateral
+      elsif (@s1 == @s2) || (@s2 == @s3)|| (@s1 == @s3)
+      :isoceles
+      elsif (@s1 != @s2) && (@s2 != @s3) && (@s1 != @s3)
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  
+end
   
   
 end
