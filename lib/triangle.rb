c806a75eeacf705cb5f4ba4054c26c20d5b9c983
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-27 01:17:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     else
       if (@s1 == @s2) &&(@s2 == @s3)
         :equilateral
-      elsif (@s1 == @s2) || (@s2 == @s3)
+      elsif (@s1 != @s2) || (@s2 == @s3)
       :isoceles
       elsif (@s1 != @s2) && (@s2 != @s3) && (@s1 != @s3)
       :scalene
